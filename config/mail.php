4ae25a8b366e45d69b782ae691337e6a7d2a5304
nurik3544 <63069646+nurik3544@users.noreply.github.com>
2023-10-12 22:47:15 +0500

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'nurxoniddin@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'xxxxxxx@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'E-com Website'),
     ],
 
